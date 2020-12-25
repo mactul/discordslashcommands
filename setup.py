9aa4bc93db53bc6b7638af6b3f8c715da02899b6
= <mactulgames@gmail.com>
2020-12-25 21:07:39 +0000

    pypi config

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 
 setup(
-    name = 'discordcustomcommand',
-    packages = ['discordcustomcommand'],
+    name = 'discordslashcommands',
+    packages = ['discordslashcommands'],
     version = '1.0.0',
     license='MIT',
     description = 'A simple libary to configurate slash commands on discord',
     author = 'Macéo Tuloup',
     author_email = 'mactulgames@gmail.com',
     url = 'https://github.com/',
-    download_url = 'https://github.com/eagle-discordbot/Eagle-Api/archive/master.tar.gz',
+    download_url = 'https://github.com/mactul/discord_slash_command.git',
     keywords = ['discord', 'slash', 'custom', 'command', 'commands'],
     classifiers=[
         'Development Status :: 3 - Alpha', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
