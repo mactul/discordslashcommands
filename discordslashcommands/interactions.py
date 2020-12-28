bdfc2183c2e131128ac43f36fbed947fa745fb9f
= <mactulgames@gmail.com>
2020-12-28 20:41:45 +0000

    PEP-8 modifications

--- discordslashcommands/interactions.py
+++ discordslashcommands/interactions.py
@@ -5,13 +5,13 @@ import discord
 from datetime import datetime, tzinfo, timedelta
 
 
-
 class simple_utc(tzinfo):
     """
     a class used by the libary
     """
-    def tzname(self,**kwargs):
+    def tzname(self, **kwargs):
         return "UTC"
+
     def utcoffset(self, dt):
         return timedelta(0)
 
@@ -34,37 +34,35 @@ class Interaction:
         if "options" in data:
             for option in data["options"]:
                 self.command.add_option(ReturnedOption(name=option["name"], value=option["value"]))
-    
+
+
     def __str__(self):
         return str(self.command)
-    
-    
-    
+
+
     def call_on_message(self, prefix):
         """
         generate a fake discord.Message object and call the on_message function with him
         with this function, you can adapt your old bot who work with messages
         """
         message = {
-  "reactions": [],
-  "attachments": [],
-  "tts": False,
-  "embeds": [],
-  "timestamp": str(datetime.utcnow().replace(tzinfo=simple_utc()).isoformat()),
-  "mention_everyone": False,
-  "id": str(self.id),
-  "pinned": False,
-  "edited_timestamp": None,
-  "author": self._member_data["user"],
-  "member": self._member_data,
-  "mention_roles": [],
-  "content": prefix + str(self.command),
-  "channel_id": str(self.channel.id),
-  "mentions": [],
-  "type": 0
-}
-        
+            "reactions": [],
+            "attachments": [],
+            "tts": False,
+            "embeds": [],
+            "timestamp": str(datetime.utcnow().replace(tzinfo=simple_utc()).isoformat()),
+            "mention_everyone": False,
+            "id": str(self.id),
+            "pinned": False,
+            "edited_timestamp": None,
+            "author": self._member_data["user"],
+            "member": self._member_data,
+            "mention_roles": [],
+            "content": prefix + str(self.command),
+            "channel_id": str(self.channel.id),
+            "mentions": [],
+            "type": 0
+        }
+
         message = discord.Message(state=self.channel._state, channel=self.channel, data=message)
         self._client.loop.create_task(__main__.on_message(message))
-        
-        
